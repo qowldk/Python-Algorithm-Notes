83c6d649e81fb8a5ea20d9a02505e6f9af6b2c9e
BaeJia <124412137+qowldk@users.noreply.github.com>
2024-01-18 22:07:35 +0900

    Create 10974.py

---
+++ baekjoon/BackTracking/10974.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+temp = []
+
+def dfs() :
+  if len(temp) == N :
+    print(*temp)
+    return 
+
+  for i in range(1, N+1) :
+    if i not in temp :
+      temp.append(i)
+      dfs()
+      temp.pop()
+
+dfs()
