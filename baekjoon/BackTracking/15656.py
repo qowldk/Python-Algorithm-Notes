b43474b5fec704d968a6cb62805f1bbddb5fd12f
BaeJia <124412137+qowldk@users.noreply.github.com>
2024-01-18 22:06:37 +0900

    Create 15656.py

---
+++ baekjoon/BackTracking/15656.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+
+s = []
+
+def dfs() :
+  if len(s) == M :
+    print(' '.join(map(str, s)))
+    return
+
+  for i in range(1, N + 1) :
+    s.append(arr[i-1])
+    dfs()
+    s.pop()
+
+dfs()
